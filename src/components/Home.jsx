cec33082f85ab788a5342856a4866fb25dbfe101
Ayesha Mulani <ayeshamulani928@gmail.com>
2024-04-19 20:20:09 +0530

    React App setup

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-gray-300 hover:text-white p-10">Home</h1>
+    </div>
+  )
+}
+
+export default Home
