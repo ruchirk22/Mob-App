01bd80ea441b1a283d97605a6fdb5d8caa4fa457
Ayesha Mulani <ayeshamulani928@gmail.com>
2024-04-19 20:26:14 +0530

    React App Setup

---
+++ src/components/Payment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div>
+      <h1 className="text-gray-300 hover:text-white p-10">Payment</h1>
+    </div>
+  )
+}
+
+export default Payment
